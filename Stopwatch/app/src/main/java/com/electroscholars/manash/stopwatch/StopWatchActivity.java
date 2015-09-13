6a2b8160573b23c5a6a6336f023eb6ce84db1122
Manash Mandal <manashmndl@gmail.com>
2015-09-13 12:37:05 +0600

    formatted code

--- Stopwatch/app/src/main/java/com/electroscholars/manash/stopwatch/StopWatchActivity.java
+++ Stopwatch/app/src/main/java/com/electroscholars/manash/stopwatch/StopWatchActivity.java
@@ -1,3 +1,9 @@
+/*
+
+    Author: Manash
+    
+ */
+
 package com.electroscholars.manash.stopwatch;
 
 import android.app.Activity;
@@ -72,7 +78,7 @@ public class StopWatchActivity extends Activity {
                 timeView.setText(time);
                 if (running){ seconds++; }
                 handler.postDelayed(this, 1000);
-                
+
             }
         });
     }
