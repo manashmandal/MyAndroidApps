227cba9508cd7b1f3e870ae2f79ea773a0ddf02e
Manash Mandal <manashmndl@gmail.com>
2015-09-16 20:50:50 +0600

    instance onStart onStop added

--- Stopwatch/app/src/main/java/com/electroscholars/manash/stopwatch/StopWatchActivity.java
+++ Stopwatch/app/src/main/java/com/electroscholars/manash/stopwatch/StopWatchActivity.java
@@ -21,6 +21,7 @@ public class StopWatchActivity extends Activity {
 
     private int seconds = 0;
     private boolean running;
+    private boolean wasRunning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +30,7 @@ public class StopWatchActivity extends Activity {
         if(savedInstanceState != null){
             seconds = savedInstanceState.getInt("seconds");
             running = savedInstanceState.getBoolean("running");
+            wasRunning = savedInstanceState.getBoolean("wasRunning");
         }
         runTimer();
     }
@@ -90,5 +92,21 @@ public class StopWatchActivity extends Activity {
     public void onSaveInstanceState(Bundle savedInstanceState){
         savedInstanceState.putInt("seconds", seconds);
         savedInstanceState.putBoolean("running", running);
+        savedInstanceState.putBoolean("wasRunning", wasRunning);
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        wasRunning = running;
+        running = false;
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        if (wasRunning){
+            running = true;
+        }
     }
 }
